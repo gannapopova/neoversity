335cd72d071dbf96b9f8a7f947091fc147257afe
Ganna Popova <ganna.popova283@gmail.com>
2026-02-22 12:04:44 +0100

    Homework 8

---
+++ pycore_hw_8/task_1/main.py
@@ -0,0 +1,21 @@
+def caching_fibonacci():
+    cache = {}
+    
+    def fibonacci(n):
+        if n <= 0:
+            return 0
+        if n == 1:
+            return 1
+        
+        if n in cache:
+            return cache[n]
+        
+        cache[n] = fibonacci(n - 1) + fibonacci(n - 2)
+        return cache[n]
+    
+    return fibonacci
+
+fib = caching_fibonacci()
+
+print(f"fib(10) = {fib(10)}")
+print(f"fib(15) = {fib(15)}")
